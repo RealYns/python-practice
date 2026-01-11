f69bdd2760fbfe924c0c804dd406d94768b1fe29
RealYns <imtherealyns@gmail.com>
2026-01-11 15:42:59 +0100

    new file apostrophe.py

--- name.py
+++ name.py
@@ -10,3 +10,25 @@ name = "Youness Shami"
 
 print(name.lower())
 
+first_name = "Youness"
+
+family_name = "Shami"
+
+full_name = first_name + " " + family_name
+
+greeting_message = "Hello, " + full_name.title() + "!"
+
+print(greeting_message)
+
+print("\tPython\tC\tJavascript")
+
+print("Python\nC\nJavascript")
+
+print("\tPython\n\tC\n\tJavascript")
+
+language = " python "
+
+language = language.strip()
+
+print(language)
+
