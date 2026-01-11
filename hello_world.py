9d3a2315c184bd1414f59b7f82fd78e7b60d1a74
RealYns <imtherealyns@gmail.com>
2026-01-11 15:07:55 +0100

    Add initial files

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+message = "Hello world!"
+
+print(message)
+
+message = "Hello python course world!"
+
+print(message)
---
+++ name.py
@@ -0,0 +1,12 @@
+name = "yOuNeSs sHaMi"
+
+print(name.title())
+
+name = "Youness Shami"
+
+print(name.upper())
+
+name = "Youness Shami"
+
+print(name.lower())
+
